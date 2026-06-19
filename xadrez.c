c094a626d1b67df215a9c9b8378ac6fb18e62b28
rodneynasp <rodney.nascimento@gmail.com>
2026-06-19 02:21:55 +0000

    inclusão do movimento do cavalo

--- xadrez.c
+++ xadrez.c
@@ -53,5 +53,39 @@ int main() {
         i++;
     } while (i <= casasRainha);
 
+    printf("\n");
+
+    /*
+     * CAVALO
+     * Movimento em "L":
+     * 2 casas para Baixo e 1 casa para Esquerda.
+     *
+     * Estruturas utilizadas:
+     * - FOR (loop externo)
+     * - WHILE (loop interno)
+     *
+     * O FOR controla a etapa vertical do movimento.
+     * O WHILE executa as duas movimentações para baixo.
+     * Após concluir as duas casas para baixo,
+     * o programa move uma casa para a esquerda.
+     */
+    printf("Movimento do Cavalo:\n");
+
+    int casasBaixo = 2;
+    int casasEsquerda = 1;
+    int contador;
+
+    for (i = 1; i <= 1; i++) {
+
+        contador = 1;
+
+        while (contador <= casasBaixo) {
+            printf("Baixo\n");
+            contador++;
+        }
+
+        printf("Esquerda\n");
+    }
+
     return 0;
 }
